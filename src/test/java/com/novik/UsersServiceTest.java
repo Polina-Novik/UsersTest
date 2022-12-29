71e0fff9631810108cd7f423d9298c7349922395
Polina-Novik <polinochka_novik_2002@mail.ru>
2022-12-29 12:09:54 +0300

    Edited tests for UsersService

--- src/test/java/com/novik/UsersServiceTest.java
+++ src/test/java/com/novik/UsersServiceTest.java
@@ -10,54 +10,55 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsersServiceTest {
-    public static final String NAME = "Polina";
-    private List<Users> users = new ArrayList<>();
-    private final UsersService usersService = new UsersService(users);
-    public static final LocalDate LOCAL_DATE = LocalDate.of(2002, 5, 28);
-    public static final LocalDate WRONG_DATE = LocalDate.of(2002, 6, 29);
+    private static final String NAME = "Polina";
+
+    private final UsersService usersService = new UsersService(new ArrayList<>());
+    private static final LocalDate LOCAL_DATE = LocalDate.of(2002, 5, 28);
+    private static final LocalDate WRONG_DATE = LocalDate.of(2002, 6, 29);
     private Users testUser = new Users("Polina", LocalDate.of(2002, 5, 28));
-    public static final String NULL_NAME = null;
-    public static final String BLANK_NAME=" ";
-    public static final LocalDate NULL_DATE = null;
+    private static final String NULL_NAME = null;
+    private static final String BLANK_NAME=" ";
+    private static final LocalDate NULL_DATE = null;
     private Users nullUser = null;
     private Users nullDateUser=new Users("Polina",null);
 
 
 
 
-    @SneakyThrows
+
     @Test
-    void createNewUser() {
+    void createNewUser() throws Exception {
      usersService.createNewUser(NAME,LOCAL_DATE);
 assertEquals(testUser.name,usersService.getUsers().get(0).name);
 assertEquals(testUser.dateOfBirth,usersService.getUsers().get(0).dateOfBirth);
     }
-    @SneakyThrows
+
     @Test
-    void removeUser() {
+    void removeUser() throws Exception {
        usersService.createNewUser(NAME,LOCAL_DATE);
         usersService.removeUser(NAME);
-        assertEquals(true,usersService.getUsers().isEmpty());
+        assertTrue(usersService.getUsers().isEmpty());
     }
 
 
-    @SneakyThrows
+
     @Test
-    void isBirthDay() {
+    void isBirthDay() throws Exception {
         usersService.createNewUser(NAME,LOCAL_DATE);
-        assertEquals(true, usersService.isBirthDay(users.get(0),LOCAL_DATE));
-       assertEquals(false,usersService.isBirthDay(users.get(0),WRONG_DATE));
+
+        assertTrue(usersService.isBirthDay(usersService.getUsers().get(0),LOCAL_DATE));
+       assertFalse(usersService.isBirthDay(usersService.getUsers().get(0),WRONG_DATE));
     }
 
-    @SneakyThrows
+
     @Test
-    void isBirthDayWithNull() {
+    void isBirthDayWithNull() throws Exception {
         usersService.getUsers().add(nullUser);
         usersService.getUsers().add(nullDateUser);
         usersService.createNewUser(NAME,LOCAL_DATE);
-        assertThrows(CustomFieldException.class,() ->  usersService.isBirthDay(users.get(0),LOCAL_DATE));
-        assertThrows(CustomFieldException.class,() ->  usersService.isBirthDay(users.get(1),LOCAL_DATE));
-        assertThrows(CustomFieldException.class,() ->  usersService.isBirthDay(users.get(2),NULL_DATE));
+        assertThrows(CustomFieldException.class,() ->  usersService.isBirthDay(usersService.getUsers().get(0),LOCAL_DATE));
+        assertThrows(CustomFieldException.class,() ->  usersService.isBirthDay(usersService.getUsers().get(1),LOCAL_DATE));
+        assertThrows(CustomFieldException.class,() ->  usersService.isBirthDay(usersService.getUsers().get(2),NULL_DATE));
     }
 
     @Test
